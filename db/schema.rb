0180a047dff4957decc829c62443b2d6a6cd039b
meeniebeanie <minyen.j@gmail.com>
2016-08-02 16:52:35 +0800

    created product scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160802085107) do
+
+  create_table "products", force: :cascade do |t|
+    t.string   "product_name"
+    t.decimal  "price"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
 
 end
