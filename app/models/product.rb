136df5362e139c208287af83ee3e477f3dd20054
meeniebeanie <minyen.j@gmail.com>
2016-08-02 19:26:29 +0800

    add relationships between users and products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ApplicationRecord
   before_save { self.product_name = product_name.titlecase }
 
+  belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160802110438) do
+ActiveRecord::Schema.define(version: 20160802112448) do
 
   create_table "products", force: :cascade do |t|
     t.string   "product_name"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 20160802110438) do
     t.decimal  "price"
     t.datetime "created_at",          null: false
     t.datetime "updated_at",          null: false
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_products_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
