c373db88bd6d367c170fcef860ceba7901bc50e0
meeniebeanie <minyen.j@gmail.com>
2016-08-03 12:28:15 +0800

    added user first name and last name

--- db/schema.rb
+++ db/schema.rb
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(version: 20160802112448) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string   "first_name",             default: "", null: false
+    t.string   "last_name",              default: "", null: false
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
