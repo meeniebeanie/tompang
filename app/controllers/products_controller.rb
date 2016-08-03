014b0a52a08d7cc558b6ff1f54bbcd2dcb07877f
meeniebeanie <minyen.j@gmail.com>
2016-08-03 14:34:56 +0800

    added countries categories, form is working properly

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -70,6 +70,6 @@ class ProductsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
-      params.require(:product).permit(:product_name, :product_description, :price)
+      params.require(:product).permit(:product_name, :country, :product_description, :price)
     end
 end
